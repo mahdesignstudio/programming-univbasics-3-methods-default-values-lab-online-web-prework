c86de6fa7b2321d177881d528545f06bbd470e9f
Melissa Ramirez <melissa.ramirez08@yahoo.com>
2019-10-14 01:48:36 +0000

    Automatically backed up by Learn

--- lib/meal_choice.rb
+++ lib/meal_choice.rb
@@ -1,6 +1,6 @@
 def meal_choice
-  vegetables = ["veg1", "veg2"]
-  protein = ["meat"]
+  vegetables = [#{veg1}, {veg2}]
+  meat = [#{protein}]
   
   puts "A plate of #{protein} with #{veg1} and #{veg2}." 
   
